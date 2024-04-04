bf9ff65a7530a0970b03b7f910c9d87a01e5e08d
udara <udaraprabath98@gmail.com>
2024-04-04 10:29:27 +0530

    before host

---
+++ app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+var mysql = require('mysql');
+
+const app = express();
+
+const api = axios.create({
+     baseURL: `https://web-api-data-generator-666d4a95c768.herokuapp.com/`,
+    headers:{
+      'X-Requested-With':'XMLHttpRequest',
+      'Accept':'Applicarion/json'
+    },
+    withCredentials: true,
+  });
+
+
+app.get('/', async(req, res) => {
+    const response = await api.get('/');
+    console.log(response)
+    res.send({
+        response:response.data
+});
+});
+
+
+
+app.listen(3001)
